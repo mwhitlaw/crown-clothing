f38db373924b3dcef5213a50654a7a5944ff38fa
Mike Whitlaw <mike.whitlaw@gmail.com>
2020-01-14 10:28:48 -0500

    Add navigation to collection from collection-preview

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import CollectionItem from '../collection-item/collection-item.component'
 import './collection-preview.styles.scss'
+import {withRouter} from 'react-router-dom'
 
-const CollectionPreview = ({title, items}) => {
-  return (
-    <div className='collection-preview'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <div className='preview'>
-        {
-          items
-            .filter((item, index) => index < 4)
-            .map(item => <CollectionItem key={item.id} item={item} />)
-        }
-      </div>
+const CollectionPreview = ({title, items, routeName, match, history}) => (
+  <div className='collection-preview'>
+    <h2 className='title' onClick={() => history.push(`${match.path}/${routeName}`)}>{title.toUpperCase()}</h2>
+    <div className='preview'>
+      {
+        items
+          .filter((item, index) => index < 4)
+          .map(item => <CollectionItem key={item.id} item={item} />)
+      }
     </div>
-  )
-}
+  </div>
+)
 
-export default CollectionPreview
+export default withRouter(CollectionPreview)
