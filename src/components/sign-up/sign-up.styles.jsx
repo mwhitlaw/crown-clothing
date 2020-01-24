49fe6bfa3e7d28a6f563b32357ec1c36421ec234
Mike Whitlaw <mike.whitlaw@gmail.com>
2020-01-24 09:32:51 -0500

    Convert to styled-components: sign-up

---
+++ src/components/sign-up/sign-up.styles.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const StyledSignUp = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 380px;
+`
+
+export const StyledTitle = styled.h2`
+  margin: 10px 0;
+`
