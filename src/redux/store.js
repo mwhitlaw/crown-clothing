5dfc0f8074b995e205fdd330e3bd380fea2090de
Mike Whitlaw <mike.whitlaw@gmail.com>
2020-02-03 06:54:58 -0500

    Add root-saga and use takeLatest

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import logger from 'redux-logger'
 import rootReducer from './root-reducer'
 import {persistStore} from 'redux-persist'
 import createSagaMiddleware from 'redux-saga'
-import {fetchCollectionsStart} from './shop/shop.sagas'
+import rootSaga from './root-saga'
 
 const sagaMiddleware = createSagaMiddleware()
 const middlewares = [sagaMiddleware]
@@ -14,6 +14,6 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
-sagaMiddleware.run(fetchCollectionsStart)
+sagaMiddleware.run(rootSaga)
 
 export const persistor = persistStore(store)
